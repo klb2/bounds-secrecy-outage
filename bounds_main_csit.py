4b07daa7922ad76b3eca8e70be0dd20e0ac7c6b2
KLB <k.besser@tu-bs.de>
2020-02-14 17:38:31 +0100

    Add independent case for main CSIT

--- bounds_main_csit.py
+++ bounds_main_csit.py
@@ -13,6 +13,7 @@ def g(y, r_s, lam_x, lam_y):
 
 def lower_bound_main_csit(r_s, r_c, lam_x, lam_y):
     yopt = _yopt_lower(r_s, lam_x, lam_y)
+    print(yopt)
     return g(yopt, r_s, lam_x, lam_y)
 
 
@@ -27,14 +28,8 @@ def upper_bound_main_csit(r_s, r_c, lam_x, lam_y):
     yopt = _yopt_upper(r_s, lam_x, lam_y)
     return np.minimum(h(yopt, r_s, lam_x, lam_y), 1)
 
-#def independent_no_csit(r_s, r_c, lam_x, lam_y):
-#    def fx(x, lam):
-#        return 1. - np.exp(-lam*x)
-#    alpha = 2**r_s-2**(r_s+r_c)
-#    int1 = fx(2**(r_s+r_c)-1, lam_x)
-#    int21 = np.exp(lam_y*alpha) - (lam_y*np.exp(alpha*(lam_x+lam_y)-lam_x*(2**r_s-1)))/(lam_x+lam_y)
-#    int22 = fx(2**(r_s+r_c)-1, lam_x)*np.exp(lam_y*alpha)
-#    return int1 + int21 - int22
+def independent_main_csit(r_s, r_c, lam_x, lam_y):
+    return 1.-(lam_y*np.exp(-lam_x*(2**r_s-1)))/(lam_x+lam_y)
 
 def export_results(results, **kwargs):
     filename = "secrecy_outage_main_csit-eve_{snr_eve_db:.1f}-rs_{r_s}-lx_{lam_x}-ly_{lam_y}.dat".format(**kwargs)
@@ -42,20 +37,21 @@ def export_results(results, **kwargs):
     data.to_csv(filename, sep="\t", index=False)
 
 def main(r_s, r_c, lam_x, lam_y, snr_eve_db):
-    snr_db = np.arange(-5, 16)
+    snr_db = np.arange(-5, 16, .5)
     snr_bob = 10**(snr_db/10)
     snr_eve = 10**(snr_eve_db/10)
     lam_xt = lam_x/snr_bob
     lam_yt = lam_y/(snr_eve*2**r_s)
+    print(lam_xt>lam_yt)
     lower = lower_bound_main_csit(r_s, r_c, lam_xt, lam_yt)
     upper = upper_bound_main_csit(r_s, r_c, lam_xt, lam_yt)
-    #indep = independent_main_csit(r_s, r_c, lam_xt, lam_yt)
-    results = {"snr": snr_db, "upper": upper, "lower": lower}#, "indep": indep}
+    indep = independent_main_csit(r_s, r_c, lam_xt, lam_yt)
+    results = {"snr": snr_db, "upper": upper, "lower": lower, "indep": indep}
     export_results(results, snr_eve_db=snr_eve_db, lam_x=lam_x, lam_y=lam_y,
                    r_c=r_c, r_s=r_s)
     plt.semilogy(snr_db, lower)
     plt.semilogy(snr_db, upper)
-    #plt.semilogy(snr_db, indep)
+    plt.semilogy(snr_db, indep)
     plt.show()
 
 if __name__ == "__main__":
