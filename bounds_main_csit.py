3519fbe34686c5b7bcb3f8566f7bce8f165ead1e
KLB <k.besser@tu-bs.de>
2020-02-15 15:43:44 +0100

    Add option to vary r_s instead of SNR_Bob

--- bounds_main_csit.py
+++ bounds_main_csit.py
@@ -31,13 +31,20 @@ def upper_bound_main_csit(r_s, r_c, lam_x, lam_y):
 def independent_main_csit(r_s, r_c, lam_x, lam_y):
     return 1.-(lam_y*np.exp(-lam_x*(2**r_s-1)))/(lam_x+lam_y)
 
-def export_results(results, **kwargs):
-    filename = "secrecy_outage_main_csit-eve_{snr_eve_db:.1f}-rs_{r_s}-lx_{lam_x}-ly_{lam_y}.dat".format(**kwargs)
+def export_results(results, filename):
     data = pd.DataFrame.from_dict(results)
     data.to_csv(filename, sep="\t", index=False)
 
-def main(r_s, r_c, lam_x, lam_y, snr_eve_db):
-    snr_db = np.arange(-5, 16, .5)
+def main(r_s, r_c, lam_x, lam_y, snr_eve_db, variable="snr"):
+    if variable == "snr":
+        snr_db = np.arange(-5, 16, .5)
+        xvar = snr_db
+        filename = f"secrecy_outage_main_csit-eve_{snr_eve_db:.1f}-rs_{r_s}-lx_{lam_x}-ly_{lam_y}.dat"
+    elif variable == "r_s":
+        snr_db = 5
+        r_s = np.logspace(-3, 1, 30)
+        xvar = r_s
+        filename = f"secrecy_outage_main_csit-eve_{snr_eve_db:.1f}-bob_{snr_db:.1f}-lx_{lam_x}-ly_{lam_y}.dat"
     snr_bob = 10**(snr_db/10)
     snr_eve = 10**(snr_eve_db/10)
     lam_xt = lam_x/snr_bob
@@ -46,12 +53,11 @@ def main(r_s, r_c, lam_x, lam_y, snr_eve_db):
     lower = lower_bound_main_csit(r_s, r_c, lam_xt, lam_yt)
     upper = upper_bound_main_csit(r_s, r_c, lam_xt, lam_yt)
     indep = independent_main_csit(r_s, r_c, lam_xt, lam_yt)
-    results = {"snr": snr_db, "upper": upper, "lower": lower, "indep": indep}
-    export_results(results, snr_eve_db=snr_eve_db, lam_x=lam_x, lam_y=lam_y,
-                   r_c=r_c, r_s=r_s)
-    plt.semilogy(snr_db, lower)
-    plt.semilogy(snr_db, upper)
-    plt.semilogy(snr_db, indep)
+    results = {variable: xvar, "upper": upper, "lower": lower, "indep": indep}
+    export_results(results, filename=filename)
+    plt.semilogy(xvar, lower)
+    plt.semilogy(xvar, upper)
+    plt.semilogy(xvar, indep)
     plt.show()
 
 if __name__ == "__main__":
@@ -62,5 +68,6 @@ if __name__ == "__main__":
     parser.add_argument("-x", dest="lam_x", default=1, type=float)
     parser.add_argument("-y", dest="lam_y", default=1, type=float)
     parser.add_argument("-e", dest="snr_eve_db", type=float, default=0)
+    parser.add_argument("--variable", default="snr", type=str)
     params = vars(parser.parse_args())
     main(**params)
